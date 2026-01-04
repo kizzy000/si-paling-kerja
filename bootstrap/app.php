febd7a93dd3140beac07d0ddc09b268978bcf4f7
kizzy000 <faiz24ti@mahasiswa.pcr.ac.id>
2026-01-04 22:40:40 +0700

    progress ke sekian (terakhir)
    gara gara gabisa di deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\CheckIsLogin;
+use App\Http\Middleware\CheckLogin;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
 
-		    'checkislogin' => CheckIsLogin::class,
+		    'checkislogin' => CheckLogin::class,
 
             'checkrole' => \App\Http\Middleware\CheckRole::class,
         ]);
